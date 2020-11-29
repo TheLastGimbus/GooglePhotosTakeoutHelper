12b65f930721cc02268ae31c595437c84e83ac25
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2020-11-29 18:29:19 +0000

    Add comment

--- google_photos_takeout_helper/__main__.py
+++ google_photos_takeout_helper/__main__.py
@@ -48,7 +48,7 @@ def main():
         help='Skips the extra photos like photos that end in "edited" or "EFFECTS".'
     )
     parser.add_argument(
-        '--skip-extras-harder',
+        '--skip-extras-harder',  # Oh yeah, skip my extras harder daddy
         action='store_true',
         help='Skips the extra photos like photos like pic(1). Also includes --skip-extras.'
     )
