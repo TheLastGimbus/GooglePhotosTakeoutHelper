15ab61ba99605fdb1d2752b3bbc0747b4fcc3feb
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-11-29 19:45:00 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE/"README.md").read_text()
 
 setuptools.setup(
     name="google-photos-takeout-helper",
-    version="1.0.1",
+    version="1.1.0",
     description="Script that organizes the Google Photos Takeout archive into one big chronological folder",
     long_description=README,
     long_description_content_type='text/markdown',
