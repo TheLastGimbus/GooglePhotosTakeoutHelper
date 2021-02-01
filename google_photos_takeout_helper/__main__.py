f3295e43b42545978013bfa253166b184ed13cc2
Conrad Storz <conradstorz@gmail.com>
2021-01-31 23:29:04 -0500

    added progress indicators

--- google_photos_takeout_helper/__main__.py
+++ google_photos_takeout_helper/__main__.py
@@ -17,6 +17,8 @@ def main():
     if _os.name == 'nt':
         import win32_setctime as _windoza_setctime
 
+    from tqdm import tqdm
+    
     parser = _argparse.ArgumentParser(
         prog='Google Photos Takeout Helper',
         usage='google-photos-takeout-helper -i [INPUT TAKEOUT FOLDER] -o [OUTPUT FOLDER]',
@@ -109,7 +111,7 @@ def main():
       folder_function=lambda fo: True,
       filter_fun=lambda file: True
     ):
-        for file in dir.rglob("*"):
+        for file in tqdm(dir.rglob("*")):
             if file.is_dir():
                 folder_function(file)
                 continue
@@ -236,7 +238,8 @@ def main():
                 files_by_size[file_size].append(file)
 
         # For all files with the same file size, get their hash on the first 1024 bytes
-        for file_size, files in files_by_size.items():
+        print(f'Checking first chunks of {len(files_by_size.items())} items...')
+        for file_size, files in tqdm(files_by_size.items()):
             if len(files) < 2:
                 continue  # this file size is unique, no need to spend cpu cycles on it
 
@@ -250,7 +253,8 @@ def main():
 
         # For all files with the hash on the first 1024 bytes, get their hash on the full
         # file - if more than one file is inserted on a hash here they are certainly duplicates
-        for files in files_by_small_hash.values():
+        print(f'Deeper analysis of {len(files_by_small_hash.values())} items...')
+        for files in tqdm(files_by_small_hash.values()):
             if len(files) < 2:
                 # the hash of the first 1k bytes is unique -> skip this file
                 continue
