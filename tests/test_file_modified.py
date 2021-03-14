bce41434d01651a46637d64b5ab048f2bece99f9
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-03-14 22:30:30 +0100

    This needs to be a function

--- tests/test_file_modified.py
+++ tests/test_file_modified.py
@@ -5,10 +5,12 @@ test_assets_path = pathlib.Path('../__takeout_helper_test_data__')
 out_path = test_assets_path / 'output'
 asserts_path = test_assets_path / 'asserts'
 
-with open(asserts_path / 'file_modified.json', 'r') as f:
-    _data = json.load(f)
-    pairs = {**_data['photos'], **_data['videos']}
 
-for key in pairs:
-    file = out_path / key
-    assert file.stat().st_mtime == pairs[key]
+def test_file_modified():
+    with open(asserts_path / 'file_modified.json', 'r') as f:
+        _data = json.load(f)
+        pairs = {**_data['photos'], **_data['videos']}
+
+    for key in pairs:
+        file = out_path / key
+        assert file.stat().st_mtime == pairs[key]
