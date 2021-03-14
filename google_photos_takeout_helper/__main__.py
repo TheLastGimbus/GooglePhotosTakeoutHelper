e6b54aab895692a53004c6980bba9b26b706fdd8
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-03-14 17:11:27 +0100

    maybe this?

--- google_photos_takeout_helper/__main__.py
+++ google_photos_takeout_helper/__main__.py
@@ -48,7 +48,7 @@ def main():
     if _os.name == 'nt':
         import win32_setctime as _windoza_setctime
 
-    assert "2432" == "dupaa cipaa"
+    raise IOError("udpafdsfsadfsdaf")
     parser = _argparse.ArgumentParser(
         prog='Google Photos Takeout Helper',
         usage='google-photos-takeout-helper -i [INPUT TAKEOUT FOLDER] -o [OUTPUT FOLDER]',
