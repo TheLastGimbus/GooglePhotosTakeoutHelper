57b33c83ad9005f1f61e5e661b506307f8a4e403
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-12-17 19:22:51 +0100

    add script for generating PKGBUILD file for aur 😎

---
+++ scripts/gen-pkgbuild.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script to generate PKGBUILD files for Ałycz Linux
+txt="# Maintainer: TheLastGimbus <mateusz.soszynski@tuta.io>
+pkgname=gpth-bin
+pkgver=$(grep -oP '(?<=version: ).*' pubspec.yaml)
+pkgrel=1
+pkgdesc='Tool to help you with exporting stuff from Google Photos'
+arch=('x86_64')
+url='https://github.com/TheLastGimbus/GooglePhotosTakeoutHelper'
+license=('Apache')
+depends=()
+provides=('gpth')
+conflicts=('gpth')
+options=('!strip')
+source=('\${url}/releases/download/v\${pkgver}/gpth-v\${pkgver}-linux')
+sha256sums=('$(sha256sum "$1" | cut -d " " -f 1)')
+
+package() {
+    install -Dm755 gpth-v\${pkgver}-linux '\$pkgdir/usr/bin/gpth'
+}"
+
+echo "$txt" >PKGBUILD
