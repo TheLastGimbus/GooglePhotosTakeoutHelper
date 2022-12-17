f02156772a7b9c22422dffc27466c99f6bbaa953
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-12-17 20:11:57 +0100

    reformat

--- lib/date_extractors/guess_extractor.dart
+++ lib/date_extractors/guess_extractor.dart
@@ -25,7 +25,8 @@ final _commonDatetimePatterns = [
   ],
   // example: signal-2020-10-26-163832.jpg
   [
-    RegExp(r'(?<date>20\d{2}-(01|02|03|04|05|06|07|08|09|10|11|12)-[0-3]\d-\d{6})'),
+    RegExp(
+        r'(?<date>20\d{2}-(01|02|03|04|05|06|07|08|09|10|11|12)-[0-3]\d-\d{6})'),
     'YYYY-MM-DD-hhmmss',
   ]
 ];
