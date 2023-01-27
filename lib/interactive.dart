48d727031d6141808b011249204b99b06a6dbd64
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2023-01-28 00:46:38 +0100

    Update lib/interactive.dart

--- lib/interactive.dart
+++ lib/interactive.dart
@@ -219,10 +219,7 @@ Future<void> unzip(List<File> zips, Directory dir) async {
     try {
       await extractFileToDisk(zip.path, dir.path, asyncWrite: true);
     } catch(e) {
-      error(
-        'Error occured while unzipping file.\n'
-        'Error details: ${e}',
-      )
+      error('Error while unzipping $zip :(\n$e);
       quit(69);
     }
   }
